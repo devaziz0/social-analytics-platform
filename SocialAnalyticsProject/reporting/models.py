d5e428d7a821d2380131d4847165353c0f4da4b8
Aziz <a.attouche@esi-sba.dz>
2020-04-22 19:11:08 +0100

    add post and reports models

--- SocialAnalyticsProject/reporting/models.py
+++ SocialAnalyticsProject/reporting/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class FacebookPost(models.Model):
+    content = models.CharField(max_length=2048)
+    date = models.DateField()
+
+class InstagramPost(models.Model):
+    content = models.CharField(max_length=2048)
+    date = models.DateField()
+
+class InstagramReport(models.Model):
+    impressions = models.IntegerField()
+    engagement = models.IntegerField()
+    date = models.DateField()
+    post = models.ForeignKey(InstagramPost,on_delete=models.CASCADE)
+
+
+class FacebookReport(models.Model):
+    impressions = models.IntegerField()
+    engagement = models.IntegerField()
+    date = models.DateField()
+    post = models.ForeignKey(FacebookPost,on_delete=models.CASCADE)
+
