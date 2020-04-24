67323a8d4f00dda6689da0e0d60fa438725d698e
Aziz <a.attouche@esi-sba.dz>
2020-04-24 19:02:40 +0100

    add FacebookPage model

--- SocialAnalyticsProject/landing/models.py
+++ SocialAnalyticsProject/landing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
 # Create your models here.
+
+class FacebookPage(models.Model):
+    page_id = models.CharField(max_length=2048,unique=True)
+    account = models.ForeignKey(SocialAccount,on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=2048)
+    name =  models.CharField(max_length=248)
