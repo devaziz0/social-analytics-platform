b1ad0b3ed5c1268e3c01b506eda8b0fc0f3b6387
Aziz <a.attouche@esi-sba.dz>
2020-04-24 19:02:26 +0100

    add constants

---
+++ SocialAnalyticsProject/modules/utils/constants.py
@@ -0,0 +1,13 @@
+URL_BASE_FACEBOOK='https://graph.facebook.com/v6.0/'
+URL_PAGE_INSIGHTS='/insights'
+
+URL_PAGE_LIST = 'me/accounts'
+
+
+### The number of people who engaged with your Page. 
+### Engagement includes any click.
+METRIC_ENGAGED_USERS='page_engaged_users'
+
+### The number of times people have engaged with your posts 
+# through reactions, comments, shares and more.
+METRIC_POST_ENGAGEMENTS='page_post_engagements'
