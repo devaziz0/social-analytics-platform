30d0da4836b820c370c262507c7cac59a9e1b8d9
Aziz <a.attouche@esi-sba.dz>
2020-04-29 16:35:52 +0100

    add page engagement task https://github.com/devaziz0/social-analytics-platform/issues/1

--- SocialAnalyticsProject/modules/facebook_api/tasks.py
+++ SocialAnalyticsProject/modules/facebook_api/tasks.py
@@ -8,10 +8,14 @@ from landing.models import *
 
 
 @shared_task
-def page_engagement_task(page_pk):
+def page_engagement_task(page_pk,start_date,end_date):
     
     facebook_page = FacebookPage.objects.get(pk=page_pk)
 
-    data = get_page_insights(facebook_page.page_id, [METRIC_ENGAGED_USERS,METRIC_POST_ENGAGEMENTS],facebook_page.access_token)
+    daily_impressions = get_page_insights(facebook_page.page_id, [METRIC_PAGE_IMPRESSIONS],facebook_page.access_token,start_date,end_date)
 
-    
+    facebook_report = FacebookPageReport.objects.get(date=start_date)
+    facebook_report.impressions = daily_impressions
+    facebook_report.save()
+
+    return daily_impressions
--- SocialAnalyticsProject/modules/utils/urls.py
+++ SocialAnalyticsProject/modules/utils/urls.py
@@ -1,23 +1,37 @@
 from .constants import *
 import requests
+import datetime
+import calendar
 
-def add_access_token(url, access_token):
+def extract_data(url, access_token):
 
     PARAMS = {"access_token": access_token}
     r = requests.get(url = url, params = PARAMS) 
 
-    return r
+    data = r.json()
+
+    value = data['data'][0]['values'][0]['value']
+
+    return value
 
 
-def get_page_insights(object_id,metrics,access_token):
+def get_page_insights(object_id,metrics,access_token,start_date,end_date):
 
     url = URL_BASE_FACEBOOK+ object_id + URL_PAGE_INSIGHTS + '?metric='
     for metric in metrics:
         url +=  metric + ','
     
-    data = add_access_token(url,access_token)
+    start_timestamp = calendar.timegm(start_date.timetuple())
+    end_timestamp = calendar.timegm(end_date.timetuple())
 
-    return data.json()
+    url+='&since='+str(start_timestamp)
+    url+='&until='+str(end_timestamp)
+
+
+
+    data = extract_data(url,access_token)
+
+    return data
 
 
 def get_pages_list(access_token):
