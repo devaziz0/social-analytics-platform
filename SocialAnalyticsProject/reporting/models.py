256c25d0fe708a9dfdd71cc0caa2579b0d0a1a5c
Aziz <a.attouche@esi-sba.dz>
2020-04-29 16:34:35 +0100

    add FacebookPageReport model

--- SocialAnalyticsProject/reporting/models.py
+++ SocialAnalyticsProject/reporting/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from landing.models import *
 # Create your models here.
 class FacebookPost(models.Model):
     content = models.CharField(max_length=2048)
@@ -16,9 +16,14 @@ class InstagramReport(models.Model):
     post = models.ForeignKey(InstagramPost,on_delete=models.CASCADE)
 
 
-class FacebookReport(models.Model):
+class FacebookPostReport(models.Model):
     impressions = models.IntegerField()
     engagement = models.IntegerField()
     date = models.DateField()
     post = models.ForeignKey(FacebookPost,on_delete=models.CASCADE)
 
+class FacebookPageReport(models.Model):
+    impressions = models.IntegerField()
+    engagement = models.IntegerField()
+    date = models.DateField()
+    page = models.ForeignKey(FacebookPage,on_delete=models.CASCADE)
