26a453d07d092537b47ae0777ec4c04a207ad27e
Aziz <a.attouche@esi-sba.dz>
2020-05-10 12:40:55 +0100

    fix bug in sign up signal

--- SocialAnalyticsProject/landing/signals.py
+++ SocialAnalyticsProject/landing/signals.py
@@ -1,27 +1,27 @@
-from allauth.socialaccount.signals import pre_social_login
+from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 from modules.utils.urls import get_pages_list,get_page_posts
 from modules.utils.helpers import get_page_post_helper
 
-
-@receiver(pre_social_login)
+# Signal launched after a user is signed up and before the login occue
+@receiver(user_signed_up)
 def social_login_handler(sender, **kwargs):
     from .models import FacebookPage  # or...
     from reporting.models import FacebookPost
     
-
+	# Retrieve the social account object
     social_account = kwargs['sociallogin'].account
+    
+	# Retrieve pages that the account has
     pages_list = get_pages_list(kwargs['sociallogin'].token.token)
     
-
-    facebook_page_list = FacebookPage.objects.filter(account=social_account)
-    
     for page in pages_list['data']:
-        
-            facebook_page = FacebookPage.objects.create(
+
+        # Save each page in the database.   
+        facebook_page = FacebookPage.objects.create(
                     account=social_account, page_id=page['id'], access_token=page['access_token'], name=page['name'])
-            facebook_page.save()
+        facebook_page.save()
 
-            data = get_page_posts(facebook_page.page_id,facebook_page.access_token)
-
-            get_page_post_helper(data,facebook_page)
+        # Retrive page posts and save them in the database
+        data = get_page_posts(facebook_page.page_id,facebook_page.access_token)
+        get_page_post_helper(data,facebook_page)
