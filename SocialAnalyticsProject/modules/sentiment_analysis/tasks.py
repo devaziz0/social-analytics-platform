fbd97e4300fbe1e9316a1baed9d48d3299a66ac2
Aziz <a.attouche@esi-sba.dz>
2020-05-21 15:08:37 +0100

    add evaluate_facebook_post through sentiment analysis

--- SocialAnalyticsProject/modules/sentiment_analysis/tasks.py
+++ SocialAnalyticsProject/modules/sentiment_analysis/tasks.py
@@ -1,1 +1,17 @@
 from celery import shared_task
+from reporting.models import FacebookComment, FacebookPost
+from .sentiment_urls import predict_multiple_comments
+
+# Evaluate post comments through sentiment analysis app
+@shared_task
+def evaluate_facebook_post(post_pk):
+
+    data = {}
+
+    facebook_comment_list = list(FacebookComment.objects.filter(
+        post__pk=post_pk).values_list('content', flat=True))
+
+    if facebook_comment_list != []:
+        data = predict_multiple_comments(facebook_comment_list)
+
+    return data
