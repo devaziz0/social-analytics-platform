3acbb3eb0984c45b211d2b41e34fee6c0b05fa99
Aziz <a.attouche@esi-sba.dz>
2020-05-25 22:35:10 +0100

    add post sentiment predict url

--- SocialAnalyticsProject/reporting/urls.py
+++ SocialAnalyticsProject/reporting/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('pages/<int:page_pk>/growth/1week_monthly',views.get_1week_monthly_growth,name='this_month_vas_last_month_growth'),
     path('pages/<int:page_pk>/growth/28days_monthly',views.get_28days_monthly_growth,name='this_month_vas_last_month_growth'),
     path('pages/<int:page_pk>/growth/1day_daily',views.get_1day_daily_growth,name='today_vs_yesterday_growth'),
+    path('post/<int:post_id>/predict/',views.get_post_sentiment, name= 'get_post_sentiment')
 ]
