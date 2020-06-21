ca8f04b76f04886a2c342ead0e157a39ac1852a1
Aziz <a.attouche@esi-sba.dz>
2020-06-21 20:49:55 +0100

    add facebookpostreport to admin panel

--- SocialAnalyticsProject/reporting/admin.py
+++ SocialAnalyticsProject/reporting/admin.py
@@ -3,6 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(FacebookPageReport)
+admin.site.register(FacebookPostReport)
 admin.site.register(FacebookPage)
 admin.site.register(FacebookPost)
 admin.site.register(FacebookComment)
